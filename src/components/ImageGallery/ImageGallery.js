24833f4096ec03d8ecd311e3402144b35a04bb37
Danylo Bay <danylobay@gmail.com>
2023-04-22 13:40:05 +0200

    updates

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,61 +1,13 @@
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-
-import './ImageGallery.scss';
 import { Loader } from 'components/Loader/Loader';
 import { Button } from 'components/Button/Button';
-import PropTypes from 'prop-types';
-import fetchPhotos from 'components/API/Api';
 
+import './ImageGallery.scss';
 export class ImageGallery extends Component {
-  state = {
-    data: [],
-    status: 'idle',
-    page: 1,
-    error: null,
-    imagesOnPage: 0,
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    const { searchQuery } = this.props;
-    const { page } = this.state;
-    try {
-      if (prevProps.searchQuery !== searchQuery) {
-        this.setState({ status: 'pending' });
-        const { hits } = await fetchPhotos(searchQuery);
-        this.setState({
-          data: hits,
-          imagesOnPage: hits.length,
-          status: 'resolved',
-        });
-      }
-    } catch (error) {
-      this.setState({ error, status: 'rejected' });
-      console.log(error.message);
-    }
-
-    if (prevState.page !== page) {
-      try {
-        this.setState({ status: 'pending' });
-        const { hits } = await fetchPhotos(searchQuery, page);
-        this.setState(prevState => ({
-          data: prevState.data.concat(hits),
-          imagesOnPage: prevState.imagesOnPage + hits.length,
-          status: 'resolved',
-        }));
-      } catch (error) {
-        this.setState({ error, status: 'rejected' });
-        console.log(error.message);
-      }
-    }
-  }
-
-  incrementPage = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
-  };
-
   render() {
-    const { data, status, imagesOnPage } = this.state;
+    const { dataImages, showBtn, onClick, status } = this.props;
 
     if (status === 'idle') {
       return (
@@ -68,23 +20,21 @@ export class ImageGallery extends Component {
     }
 
     if (status === 'rejected') {
-      <div className="info">
-        Перепрошуємо за не зручності, але за вашим запитом нічого не знайдено
-      </div>;
-    }
-
-    if (status === 'resolved') {
       return (
-        <>
-          <ul className="gallery">
-            {data && <ImageGalleryItem dataImages={data} />}
-          </ul>
-          {status === 'resolved' && imagesOnPage >= 12 && (
-            <Button onClick={this.incrementPage} />
-          )}
-        </>
+        <div className="info">
+          Перепрошуємо за не зручності, але за вашим запитом нічого не знайдено
+        </div>
       );
     }
+
+    return (
+      <>
+        <ul className="gallery">
+          <ImageGalleryItem dataImages={dataImages} />
+        </ul>
+        {showBtn && <Button onClick={onClick} />}
+      </>
+    );
   }
 }
 
